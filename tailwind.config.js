432206591ed08ef0b17f2f9aaf9ca858a4fa0d62
Md. Akdas Ali <akdas5783@.com>
2023-02-28 12:46:37 +0600

    Simple website by Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// for tailwind extension use
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
